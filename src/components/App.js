f65f24e4162edb497180975269ceef823e637fe2
Kelvin Brito <britok30@gmail.com>
2020-07-30 01:38:19 -0400

    landing pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import '../App.css';
 
 const App = () => {
-    return <div></div>;
+    return (
+        <div>
+            <div id="brito">
+                <div className="intro">
+                    <h3>Brito</h3>
+                    <p>Frontend Developer</p>
+                </div>
+                
+            </div>
+        </div>
+    );
 };
 
 export default App;
