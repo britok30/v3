b424c6d9a8d993b7bb9245e19392181c3b0f7410
Kelvin Brito <britok30@gmail.com>
2022-04-23 14:20:29 -0400

    fix api params + resume link

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -1,55 +1,54 @@
-import React from 'react';
-import resume from '../images/Kelvin_Brito_-_Frontend_Engineer.pdf';
-import Fade from 'react-reveal/Fade';
+import React from "react";
+import Fade from "react-reveal/Fade";
 
 export const Connect = () => {
-    return (
-        <>
-            <Fade bottom duration={3000} distance={'1rem'}>
-                <h1 className="text-white text-4xl mt-10 text-center antialiased">
-                    Get in touch
-                </h1>
-            </Fade>
+  return (
+    <>
+      <Fade bottom duration={3000} distance={"1rem"}>
+        <h1 className="text-white text-4xl mt-10 text-center antialiased">
+          Get in touch
+        </h1>
+      </Fade>
 
-            <div className="flex justify-center items-center flex-col md:flex-row md:space-x-6  mt-3 font-light">
-                <Fade bottom cascade duration={3000} distance={'1rem'}>
-                    <a
-                        className="text-gray-400"
-                        href="https://github.com/britok30"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        Github
-                    </a>
+      <div className="flex justify-center items-center flex-col md:flex-row md:space-x-6  mt-3 font-light">
+        <Fade bottom cascade duration={3000} distance={"1rem"}>
+          <a
+            className="text-gray-400"
+            href="https://github.com/britok30"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
 
-                    <a
-                        className="text-gray-400"
-                        href="mailto:britok30@gmail.com"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        Gmail
-                    </a>
+          <a
+            className="text-gray-400"
+            href="mailto:britok30@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Gmail
+          </a>
 
-                    <a
-                        className="text-gray-400"
-                        href="https://www.linkedin.com/in/kelvin-brito/"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        Linkedin
-                    </a>
+          <a
+            className="text-gray-400"
+            href="https://www.linkedin.com/in/kelvin-brito/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Linkedin
+          </a>
 
-                    <a
-                        className="text-gray-400"
-                        href={resume}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        Resume
-                    </a>
-                </Fade>
-            </div>
-        </>
-    );
+          <a
+            className="text-gray-400"
+            href="/resume.pdf"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Resume
+          </a>
+        </Fade>
+      </div>
+    </>
+  );
 };
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,140 +1,138 @@
-import React, { useState, useEffect } from 'react';
-import { ArrowDown, Download } from 'react-feather';
-import Fade from 'react-reveal/Fade';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { ArrowDown, Download } from "react-feather";
+import Fade from "react-reveal/Fade";
+import axios from "axios";
 
 export const Intro = () => {
-    const [imageData, setImageData] = useState(null);
-    const [download, setDownload] = useState(null);
-    useEffect(() => {
-        const fetchUnsplash = async () => {
-            const response = await axios.get(
-                `https://api.unsplash.com/photos/random?client_id=${process.env.REACT_APP_API_KEY}`,
-                {
-                    content_filter: 'high',
-                    orientation: 'landscape',
-                    // Nature, Architecture, Texture/Patterns
-                    topics: '6sMVjTLSkeQ,rnSKDHwwYUk,iUIsnVtjB0Y',
-                }
-            );
+  const [imageData, setImageData] = useState(null);
+  const [download, setDownload] = useState(null);
+  const baseUrl = "https://api.unsplash.com";
 
-            setImageData(response.data);
-            console.log(response.data);
-        };
+  useEffect(() => {
+    const topics = {
+      nature: "6sMVjTLSkeQ",
+      architecture: "rnSKDHwwYUk",
+      wallpapers: "bo8jQKTaE0Y",
+      fashion: "S4MKLAsBB74",
+      experimental: "qPYsDzvJOYc",
+    };
 
-        fetchUnsplash();
-    }, []);
+    const fetchUnsplash = async () => {
+      const response = await axios.get(
+        `${baseUrl}/photos/random?client_id=${process.env.REACT_APP_API_KEY}`,
+        {
+          params: {
+            content_filter: "high",
+            orientation: "landscape",
+            topics: `${topics.nature},${topics.architecture},${topics.wallpapers},${topics.fashion},${topics.experimental}`,
+          },
+        }
+      );
 
-    useEffect(() => {
-        if (!imageData) return;
+      setImageData(response.data);
+      console.log(response.data);
+    };
 
-        const fetchDownload = async (data) => {
-            const response = await axios.get(
-                `${data.links.download_location}&client_id=${process.env.REACT_APP_API_KEY}`
-            );
+    fetchUnsplash();
+  }, []);
 
-            setDownload(response.data);
-        };
+  useEffect(() => {
+    if (!imageData) return;
 
-        fetchDownload(imageData);
-    }, [imageData]);
+    const fetchDownload = async (data) => {
+      const response = await axios.get(
+        `${data.links.download_location}&client_id=${process.env.REACT_APP_API_KEY}`
+      );
 
-    return (
-        <div className="relative h-screen max-h-screen min-w-full">
-            {imageData && (
-                <a
-                    href={`https://unsplash.com/photos/${imageData.id}`}
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <img
-                        className="opacity-50 object-cover h-screen max-h-screen min-w-full"
-                        src={imageData.urls.full}
-                        alt="hero-img"
-                        loading="eager"
-                    />
-                </a>
-            )}
-            <div
-                className="absolute flex justify-center items-center flex-col"
-                style={{
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                }}
-            >
-                <Fade bottom duration={3000} distance={'1rem'}>
-                    <h1 className="text-3xl lg:text-6xl font-light text-center mb-3 text-white antialiased">
-                        Brito
-                    </h1>
-                </Fade>
-                <Fade bottom delay={2000} duration={3000} distance={'1rem'}>
-                    <p className="text-lg font-extralight text-center mb-3 text-white antialiased">
-                        Frontend Engineer
-                    </p>
-                </Fade>
-            </div>
-            <div className="absolute text-white text-sm right-3 bottom-3 opacity-50">
-                {imageData && (
-                    <Fade bottom delay={2000} duration={3000} distance={'1rem'}>
-                        <>
-                            Photo by{' '}
-                            <a
-                                href={`https://unsplash.com/${imageData.user.username}`}
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                {imageData.user.name}
-                            </a>{' '}
-                            on{' '}
-                            <a
-                                href="https://unsplash.com"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                Unsplash
-                            </a>
-                        </>
-                    </Fade>
-                )}
-            </div>
+      setDownload(response.data);
+    };
 
-            <div className="absolute bottom-3 left-3 opacity-70 text-white">
-                {download && (
-                    <a
-                        href={download.url}
-                        download={imageData.user.name}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <Fade
-                            bottom
-                            delay={2000}
-                            duration={3000}
-                            distance={'1rem'}
-                        >
-                            <Download color="#fff" size={28} />
-                        </Fade>
-                    </a>
-                )}
-            </div>
+    fetchDownload(imageData);
+  }, [imageData]);
 
-            <Fade top delay={4000} duration={3000} distance={'1rem'}>
-                <div
-                    className="absolute flex justify-center items-center"
-                    style={{
-                        bottom: '20%',
-                        left: '50%',
-                        transform: 'translate(-50%, -50%)',
-                    }}
-                >
-                    <ArrowDown
-                        className="animate-bounce"
-                        color="#fff"
-                        size={22}
-                    />
-                </div>
+  return (
+    <div className="relative h-screen max-h-screen min-w-full">
+      {imageData && (
+        <a
+          href={`https://unsplash.com/photos/${imageData.id}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="opacity-50 object-cover h-screen max-h-screen min-w-full"
+            src={imageData.urls.full}
+            alt="hero-img"
+            loading="eager"
+          />
+        </a>
+      )}
+      <div
+        className="absolute flex justify-center items-center flex-col"
+        style={{
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <Fade bottom duration={3000} distance={"1rem"}>
+          <h1 className="text-3xl lg:text-6xl font-light text-center mb-3 text-white antialiased">
+            Brito
+          </h1>
+        </Fade>
+        <Fade bottom delay={2000} duration={3000} distance={"1rem"}>
+          <p className="text-lg font-extralight text-center mb-3 text-white antialiased">
+            Frontend Engineer
+          </p>
+        </Fade>
+      </div>
+      <div className="absolute text-white text-sm right-3 bottom-3 opacity-50">
+        {imageData && (
+          <Fade bottom delay={2000} duration={3000} distance={"1rem"}>
+            <>
+              Photo by{" "}
+              <a
+                href={`https://unsplash.com/${imageData.user.username}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {imageData.user.name}
+              </a>{" "}
+              on{" "}
+              <a href="https://unsplash.com" target="_blank" rel="noreferrer">
+                Unsplash
+              </a>
+            </>
+          </Fade>
+        )}
+      </div>
+
+      <div className="absolute bottom-3 left-3 opacity-70 text-white">
+        {download && (
+          <a
+            href={download.url}
+            download={imageData.user.name}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Fade bottom delay={2000} duration={3000} distance={"1rem"}>
+              <Download color="#fff" size={28} />
             </Fade>
+          </a>
+        )}
+      </div>
+
+      <Fade top delay={4000} duration={3000} distance={"1rem"}>
+        <div
+          className="absolute flex justify-center items-center"
+          style={{
+            bottom: "20%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <ArrowDown className="animate-bounce" color="#fff" size={22} />
         </div>
-    );
+      </Fade>
+    </div>
+  );
 };
