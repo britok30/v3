2a233dd4759296684e6868b98982f51003d35359
Kelvin Brito <britok30@gmail.com>
2022-06-03 22:39:11 -0400

    adjust width

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -23,7 +23,7 @@ export const Skills = () => {
                 Skills
             </h1>
 
-            <div className="mt-3 flex flex-col items-center justify-center md:flex-row md:space-x-2">
+            <div className="mt-3 flex flex-col items-center justify-center md:flex-row md:w-10 md:space-x-2">
                 {skills.map((skill, index) => (
                     <span
                         key={index}
